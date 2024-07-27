cd8ded2c99ee5d29fb6fce9a8ba9e5f94875be46
Artem <pomgfccdv@gmail.com>
2024-07-27 15:37:27 +0300

    a

--- app/app.py
+++ app/app.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/laba_1')
+def laba_1():
+    return render_template('laba_1.html')
+
+@app.route('/laba_2')
+def laba_2():
+    return render_template('laba_2.html')
+
+@app.route('/laba_3')
+def laba_3():
+    return render_template('laba_3.html')
+
+@app.route('/laba_4')
+def laba_4():
+    return render_template('laba_4.html')
 
 
 if __name__ == '__main__':
